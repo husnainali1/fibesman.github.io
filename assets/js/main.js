5f47ef9d14f55e55d3c1a1e7871720d72b324522
husnainali1 <48672685+husnainali1@users.noreply.github.com>
2021-08-07 14:42:59 +0500

    publishing my first site

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(".owl-carousel").owlCarousel({
+   items: 1,
+   loop: true,
+   autoplay: true,
+   autoplayTimeout: 3000,
+   autoplayHoverPause: true,
+   animateIn: "animate__slideOutLeft",
+   animateOut: "animate__slideInRight",
+   dots: true,
+   nav: false,
+});
